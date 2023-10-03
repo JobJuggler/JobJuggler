61d1671d307efc67b2cb4d0ca74947dc768cf3fa
Geoff Sun <geoffsun2@gmail.com>
2023-10-03 17:59:48 -0400

    styled dropdown

--- src/main/navBar/DropDown.tsx
+++ src/main/navBar/DropDown.tsx
@@ -9,49 +9,82 @@ const DropDown: React.FC = () => {
 
   const handleClick = () => {
     setDropdownOpen(!dropdownOpen);
-  }
+  };
 
   const navigateDashboard = () => {
     setCurrentPage('Dashboard');
+    setDropdownOpen(!dropdownOpen);
     navigate('/');
-  }
+  };
 
   const navigateCreateJob = () => {
     setCurrentPage('Add Job');
+    setDropdownOpen(!dropdownOpen);
     navigate('createjob');
-  }
+  };
 
   return (
-    <div className=''>
-      <button 
-        id='dropdownButton' 
-        data-dropdown-toggle='dropdown' 
-        className='text-tertiaryText hover:bg-black/70 rounded-lg text-sm text-center inline-flex items-center' 
-        type='button' 
-        aria-haspopup='true' 
-        aria-expanded='false' 
+    <div className='relative'>
+      <button
+        id='dropdownButton'
+        data-dropdown-toggle='dropdown'
+        className='text-tertiaryText hover:bg-black/70 rounded-lg text-sm text-center inline-flex items-center'
+        type='button'
+        aria-haspopup='true'
+        aria-expanded='false'
         onClick={handleClick}
       >
-        {currentPage} {' '} 
+        {currentPage}{' '}
         <svg className='w-2.5 h-2.5 ml-2.5 aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6'>
-          <path stroke='currentColor' strokeLinecap='round' strokeLinejoin='round' strokeWidth='2' d='m1 1 4 4 4-4'/>
+          <path
+            stroke='currentColor'
+            strokeLinecap='round'
+            strokeLinejoin='round'
+            strokeWidth='2'
+            d='m1 1 4 4 4-4'
+          />
         </svg>
       </button>
-      <div id='dropdown' className={`${dropdownOpen ? 'block' : 'hidden'} z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 `}>
-        <ul className="py-2 text-sm text-gray-700 " aria-labelledby="dropdownDefaultButton">
-        <li>
-          <a href="#" className="block px-4 py-2 hover:bg-gray-100" onClick={navigateDashboard}>Dashboard</a>
-        </li>
-        <li>
-          <a href="#" className="block px-4 py-2 hover:bg-gray-100" onClick={navigateCreateJob}>Add Job</a>
-        </li>
-        <li>
-          <a href="#" className="block px-4 py-2 hover:bg-gray-100">Sign out</a>
-        </li>
-    </ul>
+      {dropdownOpen && (
+        <div className='fixed inset-0 top-12 bg-black opacity-50' onClick={handleClick}></div>
+      )}
+      <div
+        id='dropdown'
+        className={` absolute ${
+          dropdownOpen ? 'block' : 'hidden'
+        } z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-44 `}
+      >
+        <ul
+          className='py-2 text-sm text-gray-700 '
+          aria-labelledby='dropdownDefaultButton'
+        >
+          <li>
+            <a
+              href='#'
+              className='block px-4 py-2 hover:bg-gray-100'
+              onClick={navigateDashboard}
+            >
+              Dashboard
+            </a>
+          </li>
+          <li>
+            <a
+              href='#'
+              className='block px-4 py-2 hover:bg-gray-100'
+              onClick={navigateCreateJob}
+            >
+              Add Job
+            </a>
+          </li>
+          <li>
+            <a href='#' className='block px-4 py-2 hover:bg-gray-100'>
+              Sign out
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default DropDown;
