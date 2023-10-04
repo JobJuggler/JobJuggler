525677a5c1f5d37eec333c1fce6af303a45205cb
Mnelson98 <138735614+Mnelson98@users.noreply.github.com>
2023-10-04 11:48:56 -0400

    latest navbar updates and create user functionality

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,19 @@ import './index.css';
 import Dashboard from './main/dashboard/Dashboard';
 import CreateJob from './main/createJob/CreateJob';
 import Login from './main/login/Login';
-// import NavBar from './main/navBar/NavBar.tsx';
+import SignUp from './main/login/SignUp';
+import NavBar from './main/navBar/NavBar.tsx';
 
 function App() {
+
+  const shouldDisplayNavBar = location.pathname !== '/' && location.pathname !== '/signup';
+
   return (
     <BrowserRouter>
+      {shouldDisplayNavBar && <NavBar />}
       <Routes>
         <Route path='/' element={<Login />} />
+        <Route path='/signup' element={<SignUp />} />
         <Route path='/createjob' element={<CreateJob />} />
         <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
